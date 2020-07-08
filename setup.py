63c9ea820cf40c55c7eba10bb02d2d202d747a7c
Günther Jena <guenther@jena.at>
2020-07-08 11:30:04 +0200

    fixed python versions and revisiondict version to 1.0.0

--- setup.py
+++ setup.py
@@ -22,12 +22,12 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="RevisionDict works like an ordinary dictionary with " +
                 "additional revision keeping of changes.",
@@ -42,6 +42,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/semiversus/python-revisiondict',
-    version='0.2.3',
+    version='1.0.0',
     zip_safe=False,
 )
